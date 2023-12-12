465a3ab4dcc2fcbc251f6e53e1a9d9ae94acb7e0
milankocza <kocza.milan@gmail.com>
2023-12-12 22:48:30 +0100

    lot of changes

--- tradingStrategies_v2.py
+++ tradingStrategies_v2.py
@@ -12,45 +12,90 @@ load_dotenv()
 api_key = os.getenv('API_KEY')
 database_url = os.getenv('DATABASE_URL')
 
-class fetchData:
+def calc_return(original_price, current_price):
+    return_price = (current_price-original_price)/original_price
+    return return_price
+
+class DataDownloader:
     def __init__(self, symbol, start_date, end_date):
         self.symbol = symbol
         self.start_date = start_date
         self.end_date = end_date
 
     def download_data(self):
-        self.data = yf.download(self.symbol, start = self.start_date, end = self.end_date)
-        return self.data 
-    
+        data = yf.download(self.symbol, start = self.start_date, end = self.end_date)
+        return data 
+
 class MeanReversionStrategy:
-    def __init__(self, symbol, start_date, end_date, fast_window = 20, slow_window = 50):
+    def __init__(self, symbol, start_date, end_date, investment, fast_window = 20, slow_window = 40):
         self.symbol = symbol
         self.start_date = start_date
         self.end_date = end_date
+        self.investment = investment
         self.fast_window = fast_window
         self.slow_window = slow_window
-        self.data = fetchData(self.symbol, self.start_date, self.end_date).download_data()
+        self._buy = True
+        self._sell = False
+        self.data = DataDownloader(self.symbol, self.start_date, self.end_date).download_data()
+        self._return_Index = 0
 
     def generate_signals(self):
+
         self.data['Fast_MA'] = self.data['Close'].rolling(window = self.fast_window).mean()
         self.data['Slow_MA'] = self.data['Close'].rolling(window = self.slow_window).mean()
 
-        # Create a new column to signal when to enter or exit the market
-        self.data['Signal'] = 0  # 0 indicates no action
+        for index, row in self.data.iterrows():
+            if self._buy and row['Fast_MA'] > row['Slow_MA']:
+                
+                self.data.loc[index, 'Signal'] = 'Bought'
+                self._buy = False
+                self._sell = True
 
-        # Generate signals based on moving average crossovers
-        self.data.loc[self.data['Fast_MA'] > self.data['Slow_MA'], 'Signal'] = -1  # Short signal
-        self.data.loc[self.data['Fast_MA'] < self.data['Slow_MA'], 'Signal'] = 1   # Long signal
+            elif self._sell and row['Fast_MA'] < row['Slow_MA']:
+                self.data.loc[index, 'Signal'] = 'Sold'
+                self._buy = True
+                self._sell = False
+
+            elif self._sell:
+                self.data.loc[index, 'Signal'] = 'Hold'
+
+            else:
+                self.data.loc[index, 'Signal'] = 'Waiting to Buy'
+
+        self.data = self.data.drop(columns=['Open', 'High', 'Low', 'Adj Close'])
 
     def calculate_return(self):
-        # Calculate daily returns
-        self.data['Daily_Return'] = self.data['Close'].pct_change()
 
-        # Apply the signals to calculate strategy returns
-        self.data['Strategy_Return'] = self.data['Signal'].shift(1) * self.data['Daily_Return']
+        self.data['Buying_Value'] = 0
+        self.data['Selling_Value'] = 0
+        self.data['Return_PCT'] = 0
+        self.data['Investment'] = 0
+        self.data['Profit'] = 0
 
-        # Calculate cumulative returns
-        self.data['Cumulative_Strategy_Return'] = (1 + self.data['Strategy_Return']).cumprod()
+        for index, row in self.data.iterrows():
+            
+            if row['Signal'] == 'Bought':
+                current_price = row['Close']
+                self.data.loc[index, 'Buying_Value'] = current_price
+                self.data.loc[index, 'Investment'] = self.investment
+
+                self._return_Index = index
+                
+
+            elif row['Signal'] == 'Sold':
+                current_price = row['Close']
+                original_price = self.data.loc[self._return_Index, 'Close']
+                investment_amount = self.investment #self.data.loc[self._return_Index, 'Investment']
+                return_pct = calc_return(original_price, current_price)
+                profit = investment_amount * return_pct
+                total_budget = profit + investment_amount
+
+                self.data.loc[index, 'Selling_Value'] = current_price
+                self.data.loc[index, 'Return_PCT'] = return_pct
+                self.data.loc[index, 'Profit'] = total_budget
+
+                self.investment = total_budget
+                
 
     def draw_plot(self):
         plt.figure(figsize=(12, 6))
@@ -73,12 +118,13 @@ def Main():
 
     # Download historical stock price data
     symbol = "AAPL"
-    start_date = "2022-10-01"
+    start_date = "2022-01-01"
     end_date = "2023-01-01"
+    investment = 100
 
-    mrs = MeanReversionStrategy(symbol, start_date, end_date)
+    mrs = MeanReversionStrategy(symbol, start_date, end_date, investment)
     mrs.run_strategy()
-    mrs.draw_plot()
+    # mrs.draw_plot()
     mrs.save_to_file()
 
 if __name__ == "__main__":
